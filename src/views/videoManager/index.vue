7563930e6b82a9a256691607fe9006345b045a9c
Anmengfei <mf_0604@163.com>
2020-09-09 19:03:57 +0800

    修改视频监控样式

--- src/views/videoManager/index.vue
+++ src/views/videoManager/index.vue
@@ -7,54 +7,54 @@
                 <div class="videoArea" v-if="videoOne">
                     
                     
-                    
+                    <!-- <dv-border-box-8> -->
                         <div class="oneArea" id="one" >
-                            <dv-border-box-8>
+                            
                             <img src="../../assets/videoManager/1.png" />
-                            </dv-border-box-8> 
+                            
                         </div>
-                    
+                    <!-- </dv-border-box-8>  -->
                 </div>
 
                 <!-- 两个视频 -->
                 <div class="twoVideoArea" v-if="videoTwo">
-                    <dv-border-box-8>
+                    <!-- <dv-border-box-8> -->
                         <div class="twoArea" id="two1" >
                             <img src="../../assets/videoManager/1.png" />
                         </div>
-                    </dv-border-box-8> 
-                     <dv-border-box-8>
+                    <!-- </dv-border-box-8> 
+                     <dv-border-box-8> -->
                         <div class="twoArea" id="two2" >
                             <img src="../../assets/videoManager/2.png" />
                         </div>
-                    </dv-border-box-8> 
+                    <!-- </dv-border-box-8>  -->
                 </div>
 
                 <!-- 四个视频 -->
                 <div class="fourVideoArea" v-if="videoFour">
                     <div class="fourVideoArea_1">
-                        <dv-border-box-8>
+                        <!-- <dv-border-box-8> -->
                             <div class="fourArea" id="four1" >
                                 <img src="../../assets/videoManager/1.png" />
                             </div>
-                        </dv-border-box-8> 
-                        <dv-border-box-8>
+                        <!-- </dv-border-box-8> 
+                        <dv-border-box-8> -->
                             <div class="fourArea" id="four2" >
                                 <img src="../../assets/videoManager/2.png" />
                             </div>
-                        </dv-border-box-8> 
+                        <!-- </dv-border-box-8>  -->
                     </div>
                     <div class="fourVideoArea_1">
-                        <dv-border-box-8>
+                        <!-- <dv-border-box-8> -->
                             <div class="fourArea" id="four3" >
                                 <img src="../../assets/videoManager/3.png" />
                             </div>
-                        </dv-border-box-8> 
-                        <dv-border-box-8>
+                        <!-- </dv-border-box-8> 
+                        <dv-border-box-8> -->
                             <div class="fourArea" id="four4" >
                                 <img src="../../assets/videoManager/4.png" />
                             </div>
-                        </dv-border-box-8> 
+                        <!-- </dv-border-box-8>  -->
                     </div>
                     
                     
@@ -86,11 +86,21 @@
                     </div>
                 </el-row>
                 <el-row>
-                    <dv-border-box-7>
-                        <div class="treeController">
-                            <el-tree :data="treeData"  @node-click="handleNodeClick"></el-tree>
-                        </div>
-                    </dv-border-box-7>
+                    <div class="fenleiController">
+                        <dv-border-box-7>
+                            <div class="treeController">
+                                <!-- <el-tree :data="treeData"  @node-click="handleNodeClick"></el-tree> -->
+                                <el-tree :data="treeData" node-key="id" default-expand-all @node-click="handleNodeClick">
+                                    <span class="custom-tree-node" slot-scope="{ node, data }">
+                                        <span>
+                                            <i :class="data.icon"></i>{{ node.label }}
+                                        </span>              
+                                    </span>
+                                </el-tree>
+                            </div>
+                        </dv-border-box-7>
+                    </div>
+                    
                     
                 </el-row>
             </el-col>
@@ -109,11 +119,65 @@ export default {
             treeData: [{
                 label: '石家庄智慧工地项目一期',
                 children: [
-                    {label: '大门'},
+                    {
+                        id: 1,
+                        label: '大门',
+                        icon: 'el-icon-video-camera' 
+                    },
+                    {
+                        id: 2,
+                        label: '人行通道',
+                        icon: 'el-icon-video-camera' 
+                    },
+                    {
+                        id: 3,
+                        label: '大门口',
+                        icon: 'el-icon-video-camera' 
+                    },
+                    {
+                        id: 4,
+                        label: '东南角',
+                        icon: 'el-icon-video-camera' 
+                    },
+
+                    {
+                        id: 5,
+                        label: '西南角',
+                        icon: 'el-icon-video-camera' 
+                    },
+                    {
+                        id: 6,
+                        label: '钢筋加工区',
+                        icon: 'el-icon-video-camera' 
+                    },
+
+                    {
+                        id: 7,
+                        label: '南大门外',
+                        icon: 'el-icon-video-camera' 
+                    },
+                    {
+                        id: 8,
+                        label: '中间通道',
+                        icon: 'el-icon-video-camera' 
+                    },
+                    {
+                        id: 9,
+                        label: '南侧大门内',
+                        icon: 'el-icon-video-camera' 
+                    },
+                    {
+                        id: 10,
+                        label: '西侧大门',
+                        icon: 'el-icon-video-camera' 
+                    },
+                    {
+                        id: 11,
+                        label: '东侧大门',
+                        icon: 'el-icon-video-camera' 
+                    }
                     
-                    {label: '门口'},
-                    {label: '东南角'},
-                    {label: '西南角'}
+                    
                     ]
                 }],
 
@@ -156,12 +220,13 @@ export default {
    
 }
 .videoArea {
-    /* background-image: url("../../assets/videoManager/bg_chang.png"); 
-    background-size:  100%; 
-    background-repeat: no-repeat; */
     
+    display: flex;
+    justify-content: center;
+    align-items: center; 
     height: 10rem;
     animation: daxiao_1 1s linear;
+    margin: 0 auto;
    
 }
 @keyframes daxiao_1 {
@@ -185,6 +250,9 @@ export default {
     justify-content: center;
     align-items: center; 
    margin: 0 auto;
+   margin-right: 0.6rem;
+    margin-left: 0.5rem;
+   border:1px solid rgb(223, 187, 187); 
     
 }
 
@@ -192,7 +260,7 @@ export default {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    height: 10rem;
+    height: 9rem;
     animation: daxiao_1 1s linear;
     
 }
@@ -203,7 +271,9 @@ export default {
     height: 100%;
     width: 100%;
     margin: 0 auto;
-    margin-right: 1rem;
+    margin-right: 0.6rem;
+    margin-left: 0.5rem;
+   border:1px solid rgb(223, 187, 187); 
 
 }
 .fourVideoArea {
@@ -226,7 +296,9 @@ export default {
     height: 100%;
     width: 80%;
     margin: 0 auto;
-    border:1px solid red;
+    margin-right: 0.6rem;
+    margin-left: 0.5rem;
+    border:1px solid rgb(223, 187, 187); 
 }
 .fenge {
     background: transparent;
@@ -290,11 +362,16 @@ export default {
 .sp_4_select {
     background: url("../../assets/videoManager/four_select.png") no-repeat;
 }
+.fenleiController {
+    margin-top: -0.5rem;
+   
+}
 .treeController {
     /* background-image: url("../../assets/videoManager/bg_chang.png");  */
     /* background-size:  50% 100%;  */
     /* background-repeat: no-repeat; */
-    height: 5rem;
+     height: 6rem;
+    
     
 }
  .el-tree {
@@ -310,6 +387,11 @@ export default {
   }
   .el-tree--highlight-current .el-tree-node.is-current>.el-tree-node__content {
     background-color: #66b1ff87;
+  }
+  .el-icon-video-camera {
+      color: rgb(230, 92, 92);
+      font-size: 13px;
+      padding-right:8px; 
   }
 </style>
 
