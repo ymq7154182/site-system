896e68be586c80239596fa2e9c64695ea26991cb
ymq <king3410@qq.com>
2020-10-07 14:44:33 +0800

    页面传参参数为siteId

--- src/layout/components/head/header.vue
+++ src/layout/components/head/header.vue
@@ -138,7 +138,7 @@
         gotoPage() {
           // this.textStyle = '项目概览'
 
-          this.$router.push('/home')
+          this.$router.push('/')
           // this.textStyle = this.$store.state.textStyle
 
         },
